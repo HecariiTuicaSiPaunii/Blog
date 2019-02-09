aec948e83751b7111fdf8340d4c0970abf3550d7
Hecării, Țuica și Păunii <46106346+HecariiTuicaSiPaunii@users.noreply.github.com>
2019-02-09 22:18:48 +0200

    Update navbar.js

--- script/navbar.js
+++ script/navbar.js
@@ -1,11 +1,12 @@
 window.onscroll = function() {adjustNavbar()};
+window.onresize = function() {loadsticky()};
+var header = document.getElementById("header");
 var navbar = document.getElementById("navbar");
 var sticky = 0;
 
 function loadsticky() {
-  sticky = navbar.offsetTop;
+  sticky = header.offsetHeight;
 }
-
 function adjustNavbar() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
