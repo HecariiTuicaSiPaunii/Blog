f84a6f6fd78cb9620390fba3f13b8453dadacb14
Hecării, Țuica și Păunii <46106346+HecariiTuicaSiPaunii@users.noreply.github.com>
2019-02-10 00:43:40 +0200

    Update milka.js

--- script/milka.js
+++ script/milka.js
@@ -1,1 +1,17 @@
-console.log("Milka templating engine.");
+window.onload = function() {Init();}
+
+function Init() {
+  var txt = document.body.innerHTML;
+  var ind = txt.indexOf("<!--");
+  
+  while (ind != -1) { //Parse
+    var directive = txt.substring(ind + 4, txt.indexOf("-->"));
+    
+    if (directive.substring(0, 7).toLowerCase() == "authors") {
+      directive = directive.substring(9).split(',');
+      console.log(directive);
+    }
+    txt = txt.substring(ind + 4);
+    var ind = txt.indexOf("<!--");
+  }
+}
