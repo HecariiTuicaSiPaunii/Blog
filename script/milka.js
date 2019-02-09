5ef172b70e819af36110a2c788a04af616d1f067
Hecării, Țuica și Păunii <46106346+HecariiTuicaSiPaunii@users.noreply.github.com>
2019-02-10 01:06:12 +0200

    Update milka.js

--- script/milka.js
+++ script/milka.js
@@ -4,14 +4,41 @@ function Init() {
   var txt = document.body.innerHTML;
   var ind = txt.indexOf("<!--");
   
+  if (ind != -1)
+    var newtxt = txt.substring(0, ind);
+  else
+    return;
+  
   while (ind != -1) { //Parse
-    var directive = txt.substring(ind + 4, txt.indexOf("-->"));
+    var fin = txt.indexOf("-->") + 3;
+    var directive = txt.substring(ind + 4, fin - 3);
     
-    if (directive.substring(0, 7).toLowerCase() == "authors") {
-      directive = directive.substring(9).split(',');
-      console.log(directive);
+    if (directive.substring(0, 7).toLowerCase() == "authors" || directive.substring(0, 6).toLowerCase() == "author") { //support multi-grammar
+      if (directive.substring(0, 7).toLowerCase() == "authors")
+        directive = directive.substring(9).split(',');
+      else
+        directive = directive.substring(8).split(',');
+      
+      newtxt += "<div style='align-items: center; display: flex; margin-bottom:15px;'>";
+      newtxt += "Authors:";
+      for (i = 0; i < directive.length; i++) {
+        var author = directive[i].trim();
+        console.log (author);
+        newtxt += "<img class='avatar' src='/assets/images/profiles/" + author.toLowerCase() + ".png'/>";
+      }
+      newtxt += "</div>";
     }
-    txt = txt.substring(ind + 4);
-    var ind = txt.indexOf("<!--");
+    ind = txt.indexOf("<!--");
+    if (ind != -1) {
+      console.log("Continuining " + fin + " " + ind);
+      newtxt += txt.substring(fin, ind);
+    } else {
+      console.log("Continuining " + fin);
+      newtxt += txt.substring(fin);
+      console.log (txt.substring(fin));
+    }
+    txt = txt.substring(fin);
   }
+  
+  document.body.innerHTML = newtxt;
 }
